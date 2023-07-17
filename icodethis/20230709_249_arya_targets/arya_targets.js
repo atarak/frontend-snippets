7dc4dd2ec5b7e5923d1b363f0f689056f8625c7b
atarak <me@example.com>
2023-07-17 00:37:12 -0500

    Add previous project: Arya Targets #38

--- icodethis/20230709_249_arya_targets/arya_targets.js
+++ icodethis/20230709_249_arya_targets/arya_targets.js
@@ -1,7 +1,7 @@
 const users = [
     {
         userID: 1,
-        name: "Aryana",
+        name: "Arya",
         avatar: "https://source.unsplash.com/random/900×700/?headshot&sig=1",
         lists: [
 
@@ -10,37 +10,39 @@ const users = [
                 items: [
                     {
                         itemname: "Cersei Lannister",
-                        status: false
+                        status: false,
+                        itemindex: 0
                     },
                     {
                         itemname: "The Mountain",
-                        status: false
+                        status: false,
+                        itemindex: 1
                     },
                     {
                         itemname: "Walder Frey",
-                        status: true
+                        status: true,
+                        itemindex: 2
                     }
                 ],
-                index: 0
+                index: 0,
+                maxitemIndex: 2
             }
-        ]
+        ],
+        maxListIndex: 0
     }];
 
 
-// const chatHeader = document.querySelector("#listsumm-user");
 var usersContainer = document.querySelector("#card-user-lists");
-const templateListSummary = document.querySelector('#tpl-listsummary');
-// console.log("template: " + templateListSummary.innerHTML);
 
 var templateListdesc = document.querySelector('#tpl-listdesc');
-// console.log('listdesc template: ' + templateListdesc.innerHTML);
-
 const templateListItem = document.querySelector("#tpl-listitem");
+
 var listitems = document.querySelector("#listitems");
 
 const currentUser = users[0].name;
 
 const backBtn = document.getElementById("back-btn");
+//TODO #45
 if (backBtn) {
     backBtn.addEventListener("click", clickBack, false);
 }
@@ -100,7 +102,6 @@ function exitCardList() {
 }
 
 function toggleUserList() {
-    console.log("toggling user list");
     var listdetails = document.getElementById("card-user-list");
     listdetails.classList.toggle("hide");
 
@@ -108,21 +109,34 @@ function toggleUserList() {
     userlists.classList.toggle("hide");
 }
 
-function loadProgress(number, value) {
-    document.documentElement.style
-        .setProperty('--dashoffset', (1 - value / 100) * 450 + 'px');
+function clickAddNewList() {
+    var listholder = document.getElementById("lists");
+    var desc = templateListdesc.content.cloneNode(true);
+    desc.querySelector(".listname").innerHTML = "New List";
+    var statusC = 0;
+    var number = desc.querySelector(".number");
+    number.innerHTML = 0;
+    desc.querySelector(".listsummary").addEventListener("touchstart", clickListNameToOpenItemsView, false);
+    desc.querySelector(".listsummary").addEventListener("click", clickListNameToOpenItemsView, false);
+    listholder.append(desc);
+    if (!(addToUserList(currentUser, { name: "New List", items: [] }))) {
+        console.log("could not find username: " + currentUser);
+    }
+    var percentage = 0;
+    loadProgress(number, percentage * 100);
+    console.log(users[0]);
+}
 
-    let counter = 0;
-
-    console.log("number is " + number.innerHTML);
-    setInterval(() => {
-        if (counter >= value) {
-            clearInterval;
-        } else {
-            counter += 1;
-            // number.innerHTML = `${value}%`;
-        }
-    }, 30);
+function clickAddNewItem(e) {
+    var listitems = document.getElementById("listitems");
+    var item = templateListItem.content.cloneNode(true);
+    item.querySelector(".itemname").value = "New Item";
+    item.querySelector(".itemstatusbox").checked = false;
+    item.querySelector(".itemstatus").addEventListener("onclick", clickItemStatus, false);
+    listitems.append(item);
+    var index = document.getElementById("listname-header").dataset.listindex;
+    var newitem = { itemname: "New Item", status: false };
+    addToUserListItems(currentUser, index, newitem);
 }
 
 function clickBack() {
@@ -131,25 +145,91 @@ function clickBack() {
     toggleUserList();
 }
 
-function clickAddNewList() {
-    var listholder = document.getElementsByClassName("lists")[0];
-    console.log("looking at listholder: " + listholder.innerHTML);
-    var desc = templateListdesc.content.cloneNode(true);
-    console.log(desc);
-    desc.querySelector(".listname").innerHTML = "New List";
-    var statusC = 0;
-    console.log(statusC);
-    // desc.querySelector(".listprogress").innerHTML = statusC[false] + " / " + (Number(statusC[false]) + Number(statusC[true]));
-    var number = desc.querySelector(".number");
-    number.innerHTML = 0;
-    desc.querySelector(".listsummary").addEventListener("touchstart", clickListName, false);
-    listholder.append(desc);
-    if (!(addToUserList(currentUser, { name: "New List", items: [] }))) {
-        console.log("could not find username: " + currentUser);
+function clickListNameToOpenItemsView(e) {
+    console.log("clicked list name");
+    console.log(e);
+    var listname = e;
+    if (e.target) {
+        listname = e.target.innerHTML;
+        if (e.target.querySelector(".listname")) {
+            listname = e.target.querySelector(".listname").innerHTML;
+        }
+    } else if (e.querySelector(".listname")) {
+        listname = e.querySelector(".listname").innerHTML;
     }
-    var percentage = 0;
-    loadProgress(number, percentage * 100);
-    console.log(users[0]);
+    loadUserItemList(currentUser, listname);
+}
+
+function clickItemNameToEdit(e) {
+
+}
+
+function clickItemStatus(e) {
+    var parentItem = e.parentElement;
+    console.log("changing " + e.parentElement.innerHTML + " to " + parentItem.querySelector("input").checked);
+    e.classList.toggle("completed");
+    parentItem.querySelector(".itemstatusbox").checked = !(parentItem.querySelector(".itemstatusbox").checked);
+    console.log(parentItem.querySelector("input").checked);
+
+
+}
+
+
+function loadUserLists(user) {
+
+    console.log("loading user lists");
+    console.log(users);
+    var cardlists = document.getElementById("card-user-lists");
+    console.log(cardlists);
+    var listsumm = cardlists.querySelector("#user-lists-header");
+    var useravatar = listsumm.querySelector(".user-avatar");
+    useravatar.querySelector("img").src = user.avatar;
+    useravatar.querySelector("[user-name]").innerText = user.name;
+
+    const userLists = user.lists;
+    var listholder = cardlists.querySelector("#lists");
+    listholder.innerHTML = "";
+
+
+    for (var i = 0; i < userLists.length; i++) {
+        var desc = templateListdesc.content.cloneNode(true);
+        console.log("empty desc: " + desc);
+        desc.querySelector(".listname").innerHTML = userLists[i].name;
+        var statusC = statusCounter(userLists[i].items);
+        console.log(statusC);
+        var number = desc.querySelector(".number");
+        number.innerHTML = isNaN(Number(statusC[false])) ? 0 : Number(statusC[false]);
+        desc.querySelector(".listsummary").addEventListener("touchstart", clickListNameToOpenItemsView, false);
+        listholder.append(desc);
+        var percentage = Number(statusC[false]) / ((Number(statusC[false]) + Number(statusC[true])));
+        loadProgress(number, percentage * 100);
+    }
+
+    var cardExits = listsumm.querySelector("#exit-user-lists");
+    cardExits.addEventListener("click", exitCardList, false);
+}
+
+
+function loadUserItemList(username, listname) {
+    toggleUserList();
+    var listnameheader = document.getElementById("listname-header");
+    listnameheader.value = listname;
+    var userListDetails = getUserListDetailsByName(username, listname);
+    if (userListDetails) {
+        listnameheader.dataset.listindex = userListDetails.index;
+    }
+    var itemlist = getUserListsByName(username, listname);
+    if (!(itemlist)) {
+        console.log("could not find " + listname);
+    }
+    listitems.innerHTML = "";
+    for (var i = 0; i < itemlist.length; i++) {
+        var item = templateListItem.content.cloneNode(true);
+        item.querySelector(".itemname").value = itemlist[i].itemname;
+        item.querySelector(".itemstatusbox").checked = itemlist[i].status;
+        listitems.append(item);
+    }
+    listnameheader.addEventListener("change", changeListName, false);
 }
 
 function getUserLists(username) {
@@ -162,115 +242,11 @@ function getUserLists(username) {
     return noLists;
 }
 
-function addToUserList(username, newList) {
-    var userlist = getUserLists(username);
-    if (userlist) {
-        // console.log("currently, " + userlist.length);
-        newList.index = userlist.length;
-        userlist = userlist.concat(newList);
-        // console.log("now length is " + userlist.length);
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].name == currentUser) {
-                users[i].lists = userlist;
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function clickListName(e) {
-    console.log("clicked list name");
-    var listname = e.target.innerHTML;
-    if (e.target.querySelector(".listname")) {
-        listname = e.target.querySelector(".listname").innerHTML;
-    }
-    loadUserItemList(currentUser, listname);
-}
-
-function loadUserLists(user) {
-
-    console.log("loading user lists");
-    console.log(users);
-    const listsumm = templateListSummary.content.cloneNode(true);
-
-    listsumm.querySelector("img").src = user.avatar;
-    listsumm.querySelector("[user-name]").innerText = user.name;
-
-    const numLists = user.lists.length;
-
-    // add eventl listener to button
-    const listBlock = listsumm.querySelector("[listwrapper]");
-
-    const userLists = user.lists;
-    var listholder = listsumm.querySelector(".lists");
-
-    while (listholder.firstElementChild) {
-        listholder.removeChild(listholder.firstElementChild);
-    }
-    console.log("here's listholder's innerhtml");
-    console.log(listholder.innerHTML);
-    // console.log("user lists: " + listsumm.querySelector(".listname").innerHTML);
-
-    for (var i = 0; i < 1; i++) {
-        var desc = templateListdesc.content.cloneNode(true);
-        console.log("empty desc: " + desc);
-        desc.querySelector(".listname").innerHTML = userLists[i].name;
-        var statusC = statusCounter(userLists[i].items);
-        console.log(statusC);
-        var number = desc.querySelector(".number");
-        number.innerHTML = Number(statusC[false]);
-        desc.querySelector(".listsummary").addEventListener("touchstart", clickListName, false);
-        listholder.append(desc);
-        var percentage = Number(statusC[false]) / ((Number(statusC[false]) + Number(statusC[true])));
-        loadProgress(number, percentage * 100);
-    }
-
-    var cardExits = listsumm.getElementById("exit-user-lists");
-    cardExits.addEventListener("click", exitCardList, false);
-
-    // add to dom
-    // usersContainer.innerHTML = "";
-    var addnewElem = document.getElementById("addnew");
-    if (addnewElem) {
-        usersContainer.insertBefore(listsumm, addnewElem);
-    } else {
-        usersContainer.append(listsumm);
-    }
-}
-
-
-function loadUserItemList(username, listname) {
-    toggleUserList();
-    console.log("setting " + username + " and " + listname);
-    var listnameheader = document.getElementById("listname-header");
-    // listnameheader.innerHTML = listname;
-    listnameheader.value = listname;
-    listnameheader.dataset.listindex = getUserListDetailsByName(username, listname).index;
-    var itemlist = getUserListsByName(username, listname);
-    if (!(itemlist)) {
-        console.log("could not find " + listname);
-    }
-    while (listitems.firstElementChild) {
-        listitems.removeChild(listitems.firstElementChild);
-    }
-    for (var i = 0; i < itemlist.length; i++) {
-        var item = templateListItem.content.cloneNode(true);
-        item.querySelector(".itemname").innerHTML = itemlist[i].itemname;
-        item.querySelector(".itemstatus").querySelector("input").checked = itemlist[i].status;
-        listitems.append(item);
-    }
-
-    listnameheader.addEventListener("change", saveListName, false);
-}
-
 function getUserListDetailsByName(username, listname) {
     var userlists = getUserLists(username);
     if (userlists) {
         for (var i = 0; i < userlists.length; i++) {
-            console.log("comparing " + userlists[i].name + " vs " + listname);
             if (userlists[i].name == listname) {
-                console.log("found " + userlists[i].name);
                 return userlists[i];
             }
         }
@@ -286,12 +262,46 @@ function getUserListsByName(username, listname) {
     return undefined;
 }
 
-function saveListName(e) {
-    console.log(e.target.value);
-    console.log(e.target.dataset.listindex);
+
+function addToUserList(username, newList) {
+    var user = getUserDetailsByName(username);
+    if (user) {
+        if (user.lists.length == 0) {
+            newList.maxItemIndex = 0;
+            newList.index = 0;
+            user.maxListIndex = 0;
+        } else {
+            user.maxListIndex += 1;
+            newList.index = user.maxListIndex;
+            newList.maxItemIndex = newList.length;
+        }
+        user.lists = user.lists.concat(newList);
+        return true;
+    }
+    return false;
+}
+
+function addToUserListItems(username, listIndex, newItem) {
+    var userlist = getUserListByIndex(username, listIndex);
+    if (userlist) {
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].name == currentUser) {
+                users[i].lists[listIndex].items.push(newItem);
+                console.log(users[i].lists[listIndex]);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function changeListName(e) {
     setUserListName(currentUser, e.target.value, e.target.dataset.listindex);
-    console.log("save list name");
-    console.log(users);
+}
+
+function changeListItemName(e) {
+    setUserListItemName(currentUser, e.target.value, e.target.dataset.listindex);
 }
 
 function getUserListByIndex(username, listindex) {
@@ -328,16 +338,30 @@ function filterFunction() {
     var input, filter, a, i;
     input = document.getElementById("filter-lists");
     filter = input.value.toUpperCase();
-    // div = document.getElementById("myDropdown");
-    // a = div.getElementsByClassName("country-item");
     a = document.getElementsByClassName("listname");
     for (i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText || a.innerHTML;
+        var txtValue = a[i].innerHTML;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             a[i].parentElement.style.display = "";
         } else {
             a[i].parentElement.style.display = "none";
-            // a[i].style.display = "none";
         }
     }
+}
+
+function loadProgress(number, value) {
+    document.documentElement.style
+        .setProperty('--dashoffset', (1 - value / 100) * 450 + 'px');
+
+    let counter = 0;
+
+    console.log("number is " + number.innerHTML);
+    setInterval(() => {
+        if (counter >= value) {
+            clearInterval;
+        } else {
+            counter += 1;
+            // number.innerHTML = `${value}%`;
+        }
+    }, 30);
 }
